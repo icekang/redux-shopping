8f6abb268ed92817f7b05e036e0926cda0c634fa
namkang321 <namkang_chutisilp@hotmail.com>
2020-05-28 16:15:16 +0700

    edit store structure

--- src/reducers/carts.js
+++ src/reducers/carts.js
@@ -1,18 +1,44 @@
 import actions from '../actions/actionTypes';
+import _ from 'lodash';
+import { produce } from 'immer';
 // const actions = require('../actions/actionTypes');
 
 const carts = (state = [], action) => {
+    let i;
     switch (action.type) {
         case actions.ADD_ITEM:
-            return [
-                ...state,
-                {
-                    itemId: action.itemId,
-                    itemName: action.itemName,
-                },
-            ];
+            i = _.findIndex(state, ['itemId', action.itemId]);
+            if (i < 0) {
+                return [
+                    ...state,
+                    {
+                        itemId: action.itemId,
+                        itemName: action.itemName,
+                        quantity: 1,
+                    },
+                ];
+            } else {
+                return [
+                    ...state.slice(0, i),
+                    produce(state[i], (draftItem) => {
+                        draftItem.quantity = draftItem.quantity + 1;
+                    }),
+                    ...state.slice(i + 1),
+                ];
+            }
         case actions.REMOVE_ITEM:
-            return state.filter((item) => item.itemId === action.itemId);
+            i = _.findIndex(state, ['itemId', action.itemId]);
+            if (i < 0) {
+                return state;
+            } else {
+                return [
+                    ...state.slice(0, i),
+                    produce(state[i], (draftItem) => {
+                        draftItem.quantity = Math.max(0, draftItem.quantity - 1);
+                    }),
+                    ...state.slice(i + 1),
+                ];
+            }
         default:
             return state;
     }
