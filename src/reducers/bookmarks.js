5bb9b8a14a275af555c87797af65c4eb16f04f66
namkang321 <namkang_chutisilp@hotmail.com>
2020-05-28 16:19:35 +0700

    edit store structure of bookmarks

--- src/reducers/bookmarks.js
+++ src/reducers/bookmarks.js
@@ -1,16 +1,23 @@
 // const actions = require('../actions/actionTypes');
 import actions from '../actions/actionTypes';
+import { findIndex } from 'lodash';
 
 const bookmarks = (state = [], action) => {
+    let i;
     switch (action.type) {
         case actions.ADD_BOOKMARK:
-            return [
-                ...state,
-                {
-                    itemId: action.itemId,
-                    itemName: action.itemName,
-                },
-            ];
+            let i = findIndex(state, ['itemId', action.itemId]);
+            if (i < 0) {
+                return [
+                    ...state,
+                    {
+                        itemId: action.itemId,
+                        itemName: action.itemName,
+                    },
+                ];
+            } else {
+                return state;
+            }
         default:
             return state;
     }
